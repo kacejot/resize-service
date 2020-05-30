b217b693cade2e6d67ac98c7bc08d0ac7fcb490f
Max Goncharenko <kacejot@fex.net>
2020-05-30 14:40:25 +0300

    add gqlgen; add GraphQL API description

---
+++ pkg/api/graph/schema.resolvers.go
@@ -0,0 +1,33 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/kacejot/resize-service/pkg/api/graph/generated"
+	"github.com/kacejot/resize-service/pkg/api/graph/model"
+)
+
+func (r *mutationResolver) Resize(ctx context.Context, image model.ImageInput, width int, height int) (*model.ResizeResult, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) ResizeExisting(ctx context.Context, id string, width int, height int) (*model.ResizeResult, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) ListProcessedImages(ctx context.Context) ([]*model.ResizeResult, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
---
+++ pkg/api/server.go
@@ -0,0 +1,31 @@
+package main
+
+//go:generate go run github.com/99designs/gqlgen
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/kacejot/resize-service/pkg/api/graph"
+	"github.com/kacejot/resize-service/pkg/api/graph/generated"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
